83991597aef270c04591f477a40199b03e2d2ce7
Darko Mikulic <dmikulicwr@gmail.com>
2012-03-26 01:26:00 +0200

    Better render-to-response

--- src/hello_www/routes.clj
+++ src/hello_www/routes.clj
@@ -9,17 +9,17 @@
 (defroutes main-routes
   (GET "/" [] (index-page))
 
-  ;;(GET "/user/uid-:id" [id] (get-id id))  ;;OR using a regular expression checker
+  ;;(GET "/user/uid-:id" [id] (get-id id))
   (GET ["/user/uid-:id" :id #"[0-9]+"] [id] (hello-www.views/get-id id))
 
-  (GET "/form/" []     (hello-www.views/user-form)) ;; can be written as a multi-method
-  (POST "/form/"[name] (hello-www.views/process-user-form name))
+  (GET "/form/" []     (hello-www.views/user-form))
+  (POST "/form/"[name] (hello-www.views/user-form name))
 
   (route/resources "/static/")
   (route/not-found "Page not found"))
 
 
 (def app (handler/site main-routes))
-(defn -main [];;port
+(defn -main []
   ;;(run-jetty app {:port (Integer. port)})
   (run-jetty app {:port 8080}))
--- src/hello_www/views.clj
+++ src/hello_www/views.clj
@@ -1,18 +1,18 @@
 (ns hello-www.views
   (:use clostache.parser))
 
+(defn render-to-response
+  ([template] (render (slurp (str "resources/templates/" template)) {}))
+  ([template values] (render (slurp (str "resources/templates/" template)) values)))
 
-(defn render-to-response [template values]
-  (render (slurp (str "resources/templates/" template)) values))
+;; VIEWS
 
 (defn index-page []
   (render-to-response "index.html" {:var "Hello, world!"}))
 
 (defn get-id [id]
-  (render-to-response "index.html" {:var (str "Hello: " id)}))
+  (render-to-response "index.html" {:var (str "User ID: " id)}))
 
-(defn user-form []
-  (render-to-response "form.html" {}))
-
-(defn process-user-form [name]
-  (render-to-response "index.html" {:var (str "Thanks " name)}))
+(defn user-form
+  ([]     (render-to-response "form.html"))
+  ([name] (render-to-response "index.html" {:var (str "Hello, " name)})))
